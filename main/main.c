074c1b24368fe7678edcf704ec4f4537c1ab57e0
Gaoaokai <gaoaokai9951@163.com>
2026-03-30 21:25:26 +0800

    增加E2PROM代码

--- main/main.c
+++ main/main.c
@@ -9,14 +9,89 @@
 #include "xl9555.h"
 #include "nvs_flash.h"
 #include "usart.h"
+#include "24cxx.h"
 
 //#define MY_PWM
 //#define MY_KEY
 //#define MY_XL9555
-#define MY_USART
-//#define MY_EEPROM
+//#define MY_USART
+#define MY_EEPROM
 
 #ifdef MY_EEPROM
+i2c_obj_t i2c0_master;
+
+const uint8_t g_text_buf[] = {"ESP32-S3 EEPROM"};   /* 要写入到24c02的字符串数组 */
+#define TEXT_SIZE   sizeof(g_text_buf)              /* TEXT字符串长度 */
+void app_main(void)
+{
+    uint16_t i = 0;
+    uint8_t err = 0;
+    uint8_t key;
+    uint8_t datatemp[TEXT_SIZE];
+    esp_err_t ret;
+    
+    ret = nvs_flash_init();             /* 初始化NVS */
+
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    led_init();
+    i2c0_master = iic_init(I2C_NUM_0);
+    xl9555_init(i2c0_master);           /* IO扩展芯片初始化 */
+    at24cxx_init(i2c0_master);          /* 初始化24CXX */
+
+    err = at24cxx_check();              /* 检测AT24C02 */
+    
+    if (err != 0)
+    {
+        while (1)                       /* 检测不到24c02 */
+        {
+            printf("24C02 check failed, please check!\n");
+            vTaskDelay(500);
+            LED_TOGGLE();               /* LED闪烁 */
+        }
+    }
+    printf("24C02 Ready!\n");
+    printf("\n");
+    
+    while(1)
+    {
+        key = xl9555_key_scan(0);
+        
+        switch (key)
+        {
+            case KEY0_PRES:
+            {
+                at24cxx_write(0, (uint8_t *)g_text_buf, TEXT_SIZE);
+                printf("The data written is:%s\n", g_text_buf);
+                break;
+            }
+            case KEY1_PRES:
+            {
+                at24cxx_read(0, datatemp, TEXT_SIZE);
+                printf("The data read is:%s\n", datatemp);
+                break;
+            }
+            default:
+            {
+                break;
+            }
+        }
+
+        i++;
+
+        if (i == 20)
+        {
+            LED_TOGGLE();               /* LED闪烁 */
+            i = 0;
+        }
+        
+        vTaskDelay(10);
+    }   
+}
+
 
 
 #endif
